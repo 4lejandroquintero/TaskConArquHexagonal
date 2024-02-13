ece5ae3e2947c76596a56777ca2ef7eb3d2e8e61
4lejandroquintero <alejoq96@gmail.com>
2024-02-13 10:24:31 -0500

    Comentarios

--- src/main/java/com/hexagonal/tasks/domain/ports/in/UpdateTaskUseCase.java
+++ src/main/java/com/hexagonal/tasks/domain/ports/in/UpdateTaskUseCase.java
@@ -5,5 +5,6 @@ import com.hexagonal.tasks.domain.model.Task;
 import java.util.Optional;
 
 public interface UpdateTaskUseCase {
+    //a la hora de actualizar hay que dar ID, este puede o no puede existir, usamos el optional
     Optional<Task> updateTask(Long id, Task updatedTask);
 }
